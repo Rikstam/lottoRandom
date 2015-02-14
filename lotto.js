f6372df9536c8cf41d4f71d38b10d738ff7ae640
Riku Kestilä <rikukestila@gmail.com>
2015-02-14 15:24:11 +0200

    refactor to use a function

--- lotto.js
+++ lotto.js
@@ -3,6 +3,8 @@ var _ = require('lodash');
 var express = require('express');
 var app = express();
 
+var port = process.env.PORT || 8080;
+
 // respond with "hello world" when a GET request is made to the homepage
 
 
@@ -10,36 +12,37 @@ var app = express();
 var rows, randoms, i, r, rand, seed, ints;
 //ints = [];
 
-app.get('/', function(req, res) {
+
+function createRandomLotteryRows(rows, seed){
+
 
   ints = [];
 
   rand = new gen();
-  seed = rand.cleanString("rakkaus");
+  seed = rand.cleanString(seed);
 
-  rows = 3;
+  rows = rows;
   r=1;
   i=1;
 
   //create as many rows as in the rows var;
   while(r < rows){
 
-    randoms = [];
+  randoms = [];
 
-    //create 7 ints
+  //create 7 ints
     while(i<8){
-      //randomize the seed string
+    //randomize the seed string
       seed = _.shuffle(seed);
 
-      //push the random ints to array
+    //push the random ints to array
       randoms.push(gen.create(seed));
-
       i++;
     }
 
     randoms.forEach(function(k){
 
-      console.log(k.intBetween(1,39));
+      console.log(r + " " + k.intBetween(1,39));
       randomInt = k.intBetween(1,39);
       rand.done();
       ints.push(randomInt);
@@ -48,9 +51,16 @@ app.get('/', function(req, res) {
     r++;
   }
 
-  res.send(ints);
+  return ints;
+}
 
+
+app.get('/numbers', function(req, res) {
+
+  var numbs = createRandomLotteryRows(3, "möhmö");
+  res.send(numbs);
 
 });
 
-app.listen(8080);
+app.listen(port);
+console.log('Magic happens on port ' + port);
--- readme.md
+++ readme.md
@@ -3,4 +3,4 @@
 ## What it is?
 
 A simple node.js based random number generator for generating lottery numbers for the finnish lotto ( seven numbers
-between 1 and 39)
+between 1 and 39) using node.js, express.js, lodash
